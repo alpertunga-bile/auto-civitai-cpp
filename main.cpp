d5515bde2e2e14af4b8fe94bebe3f7b8b8c07e62
alpertunga-bile <kingdom19online@hotmail.com>
2024-07-08 22:08:23 +0300

    url and parse test is done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "sources/dataset.hpp"
 
+#include "cpr/cpr.h"
+#include "nlohmann/json.hpp"
+
 #include <iostream>
 
 int
 main()
 {
+  /*
   pxd::Dataset dataset;
   dataset.init("temp.parquet");
 
@@ -13,6 +17,14 @@ main()
   dataset.add_row("kasjd", "ajhsjdhqw");
 
   dataset.write();
+  */
+
+  auto res = cpr::Get(cpr::Url(
+    "https://civitai.com/api/v1/images?limit=200&sort=Newest&period=AllTime"));
+
+  nlohmann::json data = nlohmann::json::parse(res.text);
+
+  std::cout << data["items"][0]["meta"]["prompt"];
 
   return 0;
 }
